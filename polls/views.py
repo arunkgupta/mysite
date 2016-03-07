ade60c382986f7a08e3a38ec661ff69679ff2731
arunkgupta <get.arungupta@gmail.com>
2016-03-07 16:18:03 +0530

    Generic View -> detail , result and index page

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 # from django.template import  loader
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 from .models import Question, Choice
 
 # Commented for Generic View
@@ -23,7 +24,7 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """ Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 # Commented for Generic View
@@ -38,6 +39,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 # Commented for Generic View
